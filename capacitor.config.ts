b6299feed2359a2022b4d7dbb4a076e95f2fb4c5
brock810 <130688727+brock810@users.noreply.github.com>
2026-03-03 15:36:37 -0600

    Update iOS Capacitor background color

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,17 +4,30 @@ const config: CapacitorConfig = {
   appId: 'com.quantumwebdynamics.medisynclabs.dev',
   appName: 'MediSyncLabs DEV',
   webDir: 'dist',
+
   server: {
     cleartext: true,
     allowNavigation: [
       '192.168.1.127',
-      'http://192.168.1.127:8056',
-      '192.168.1.127:8056',
+      'http://192.168.1.127:8056/manual_entry',
+      '192.168.1.127:8056/manual_entry',
       '*.medisynclabs.ca',
       'medisynclabs.ca',
       'www.medisynclabs.ca',
     ],
   },
+
+  ios: {
+    backgroundColor: '#0d6efd'
+  },
+
+  plugins: {
+    SplashScreen: {
+      backgroundColor: '#0d6efd',
+      launchAutoHide: true,
+      showSpinner: false
+    }
+  }
 };
 
 export default config;
